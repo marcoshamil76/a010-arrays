8f9a707886ec4be6ded3df07c03fc49319f5b17b
marcoshamil76 <marcoshamilton1976@gmail.com>
2022-09-01 11:59:16 -0300

    Entrega Exercicio 02 - Aula 10

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,20 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+//const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const primeiraArray = [1976, 1978, 2004, 2009]
+const segundaArray = ["Marcos", "Elida", "Maria", "Miguel", "Bento"]
+const terceiraArray = ["Marcos", 1976, true, "Elida", 1978, true, "Maria", 2004, true]
+console.log (primeiraArray.length)
+console.log (segundaArray.length)
+console.log (terceiraArray.length)
+
+
+console.log(primeiraArray[0])
+console.log(segundaArray[1])
+console.log(terceiraArray[2])
+
+console.log (primeiraArray.includes("1994"))
+
+console.log(terceiraArray.includes("Elida"))
+
+
+
